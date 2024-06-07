dd4f563b815fefa323a067d0c3c0f5ce72a3bcbe
amingma <jiama19999@gmail.com>
2024-06-07 16:53:20 -0500

    make intro disappear and make board appear on click

--- script.js
+++ script.js
@@ -7,11 +7,23 @@ function tile_small_board(small_board) {
 }
 
 function tile_all_boards() {
-    for (let i=1; i<=9; i++) {
-        const cur_id = `#board-${i}`;
-        const cur_board = document.querySelector(cur_id);
-        tile_small_board(cur_board);
+    // for (let i=1; i<=9; i++) {
+    //     const cur_id = `#board-${i}`;
+    //     const cur_board = document.querySelector(cur_id);
+    //     tile_small_board(cur_board);
+    // }
+    const boards = document.querySelectorAll('.small-board');
+    for (let i=0; i<boards.length; i++) {
+        tile_small_board(boards[i]);
     }
 }
+
+const play_button = document.querySelector('button');
+play_button.addEventListener('click', ()=>{
+    const welcome = document.querySelector('#rules');
+    welcome.style.display = 'none';
+    const board = document.querySelector('#board');
+    board.style.display = 'grid';
+})
 
 tile_all_boards();
