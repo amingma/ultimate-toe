91551cde71cce89e32cd0030e830e265f2518c07
amingma <jiama19999@gmail.com>
2024-06-07 21:27:50 -0500

    allow players to alternate moves

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 let p1_turn;
 let game_board;
+let events;
 
 function tileSmallBoard(small_board, j) {
     for (let i=0; i<9; i++) {
@@ -109,30 +110,73 @@ function shiftBold() {
     }
 }
 
-function clickSquare(cur_tile) {
-
+function makePromise(cur_tile) {
+    return new Promise((resolve)=>{
+        cur_tile.addEventListener('click', () => {
+            const ctest = cur_tile;
+            const move = document.createElement('img');
+                if (p1_turn) {
+                    move.src = './img/letter-x.svg';
+                    ctest.appendChild(move);
+                }
+                else {
+                    move.src = './img/letter-o.svg';
+                    ctest.appendChild(move);
+                }
+                resolve();
+        }, {once: true});
+    });
 }
 
-function addListeners() {
-    for (let i=0; i<81; i++) {
+function addListeners(events, restrict) {
+    let lb = restrict*9;
+    let ub = lb+9;
+    if (restrict==9) {
+        lb = 0; ub = 81;
+    }
+    for (let i=lb; i<ub; i++) {
         let cur_id = `#tile-${i}`;
         const cur_tile = document.querySelector(cur_id);
-        cur_tile.addEventListener('click', clickSquare(cur_tile));
+        if (cur_tile.hasChildNodes()) {
+            continue;
+        }
+        //cur_tile.addEventListener('click', clickSquare(cur_tile));
+        events.push(makePromise(cur_tile));
     }
 }
 
-function playRound() {
+function removeListeners(restrict) {
+    let lb = restrict*9;
+    let ub = lb+9;
+    if (restrict==9) {
+        lb = 0; ub = 81;
+    }
+    for (let i=lb; i<ub; i++) {
+        let cur_id = `#tile-${i}`;
+        const cur_tile = document.querySelector(cur_id);
+        if (cur_tile.hasChildNodes()) {
+            continue;
+        }
+        const replacement = cur_tile.cloneNode(true);
+        cur_tile.parentNode.replaceChild(replacement, cur_tile);
+    }
+}
 
+async function playGame(restrict) {
+    shiftBold();
+    events = [];
+    addListeners(events, restrict);
+    await Promise.race(events);
+    removeListeners(restrict);
+    p1_turn = !p1_turn;
+    playGame(restrict);
 }
 
 function startGame() {
     tileAllBoards();
     game_board = board();
     p1_turn = true;
-    while(isOver(board)==0) {
-        playRound();
-        p1_turn = false;
-    }
+    playGame(9);
 }
 
 startGame();
